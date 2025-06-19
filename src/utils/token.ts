8c9972967179a3d5614f845998f749d0785b8c46
shilin <39396378+shilin66@users.noreply.github.com>
2025-06-19 08:38:40 +0800

    fix: 修改 token解析方式

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -4,16 +4,29 @@ import { getJwtSignKey } from '../config';
 export const verifyToken = async (
   token: string,
 ): Promise<{ userId: string; isRoot: boolean }> => {
-  const signKey = getJwtSignKey();
   return new Promise((resolve, reject) => {
-    jwt.verify(token, signKey, (err, decoded: any) => {
-      if (err || !decoded?.userId) {
-        return reject(new Error('Invalid token'));
-      }
-      resolve({
-        userId: decoded.userId,
-        isRoot: decoded.isRoot,
-      });
-    });
-  });
+     if (!token) {
+      return reject(new Error('Invalid token'));
+    }
+     const arr = token.split(':');
+     if (arr.length !== 2) {
+      return reject(new Error('Invalid token'));
+    }
+     resolve({
+       userId: arr[0],
+       isRoot: false,
+     })
+  })
+  // const signKey = getJwtSignKey();
+  // return new Promise((resolve, reject) => {
+  //   jwt.verify(token, signKey, (err, decoded: any) => {
+  //     if (err || !decoded?.userId) {
+  //       return reject(new Error('Invalid token'));
+  //     }
+  //     resolve({
+  //       userId: decoded.userId,
+  //       isRoot: decoded.isRoot,
+  //     });
+  //   });
+  // });
 };
